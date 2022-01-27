efb6a0ebf81b3f891581779a6b5d608762940bd5
abedshamia <ab.shamia96@gmail.com>
2022-01-27 03:35:14 +0200

    Create Task Modal

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const allTasks = document.querySelector('.tasks');
-const taskInput = document.querySelector('.add-task-input');
+const taskInput = document.querySelector('.modal-task-input');
 const submitBtn = document.querySelector('.add-task-btn');
+const createTaskBtn = document.querySelector('.create-task');
 const body = document.querySelector('body');
 
 body.addEventListener('click', function (e) {
@@ -19,9 +20,10 @@ body.addEventListener('click', function (e) {
   }
 });
 
-submitBtn.addEventListener('click', function (e) {
+createTaskBtn.addEventListener('click', function (e) {
   e.preventDefault;
   createTask(e);
+  closeModal();
 });
 
 //Delete Task
@@ -84,6 +86,12 @@ function createTask() {
   taskOptions.classList.add('task-options');
   taskRow.appendChild(taskOptions);
 
+  const detailsBtn = document.createElement('i');
+  detailsBtn.classList.add('fas');
+  detailsBtn.classList.add('fa-book-open');
+  detailsBtn.classList.add('details');
+  taskOptions.appendChild(detailsBtn);
+
   const editBtn = document.createElement('i');
   editBtn.classList.add('fas');
   editBtn.classList.add('fa-edit');
@@ -96,3 +104,31 @@ function createTask() {
   deleteBtn.classList.add('delete');
   taskOptions.appendChild(deleteBtn);
 }
+
+//Modal
+
+// Get the modal
+const modal = document.getElementById('myModal');
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName('close')[0];
+
+// When the user clicks on the button, open the modal
+submitBtn.onclick = function () {
+  modal.style.display = 'block';
+};
+
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = 'none';
+};
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
